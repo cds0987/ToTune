70c82d114d12d64bfc7289def57612fd93632f53
Nguyên Trần <ttnguyen1410@gmail.com>
2026-01-06 23:25:07 +0700

    Load Peft

---
+++ Peft/lora.py
@@ -0,0 +1,11 @@
+from peft import get_peft_model, LoraConfig
+
+def load_lora_model(model, **kwargs):
+    """
+    Flexible LoRA loader.
+    Accepts any valid LoraConfig arguments via **lora_kwargs.
+    """
+    config = LoraConfig(**kwargs)
+    model = get_peft_model(model, config)
+    model.print_trainable_parameters()
+    return model
---
+++ Peft/randlora.py
@@ -0,0 +1,15 @@
+from peft import get_peft_model, RandLoraConfig
+from ToTune.models.Utils import is_4bit
+
+def load_randlora_model(model, **kwargs):
+    """
+    Flexible  loader.
+    Accepts any valid LoraConfig arguments via **lora_kwargs.
+    """
+    config = RandLoraConfig(**kwargs)
+    model = get_peft_model(model, config)
+    if is_4bit(model):
+        model = model.half()
+    model.print_trainable_parameters()
+
+    return model
