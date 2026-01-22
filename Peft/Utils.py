855f11f4ead0b6ef2bc3be2a48e752f5353446e8
Nguyên Trần <ttnguyen1410@gmail.com>
2026-01-22 17:03:33 +0700

    Fix load peft

--- Peft/Utils.py
+++ Peft/Utils.py
@@ -1,8 +1,8 @@
 from ToTune.Peft.lora import load_lora_model
 from ToTune.Peft.randlora import load_randlora_model
 
-def load_peft_model(model, **kwargs):
-    if kwargs['adaptation'] == 'lora':
+def load_peft_model(model,adaptation = 'lora', **kwargs):
+    if adaptation == 'lora':
         return load_lora_model(model, **kwargs)
-    elif kwargs['adaptation'] == 'randlora':
+    elif adaptation == 'randlora':
         return load_randlora_model(model, **kwargs)
