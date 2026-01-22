dfc41f635d8761eb78c925feeeda4b58ccfbe0c8
Nguyên Trần <ttnguyen1410@gmail.com>
2026-01-23 00:19:38 +0700

    Adalora peft type

--- Peft/adalora.py
+++ Peft/adalora.py
@@ -1,6 +1,7 @@
 from peft import LoraConfig, AdaLoraModel, AdaLoraConfig
 
 def load_adalora_model(model, **kwargs):
+    kwargs['peft_type'] = "ADALORA"
     config = AdaLoraConfig(**kwargs)
     model = AdaLoraModel(model, config, "default")
     kwargs['adaptation'] = 'adalora'
