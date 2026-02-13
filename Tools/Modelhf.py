2867eeb070b07858c9368fafa334188718ed97eb
cds0987 <ttnguyen1410@gmail.com>
2026-02-14 01:49:45 +0700

    Fix Load model

--- Tools/Modelhf.py
+++ Tools/Modelhf.py
@@ -1,4 +1,4 @@
-def save_ModelHgface(self, model, tokenizer, username, model_name, token):
+def save_ModelHgface(model, tokenizer, username, model_name, token):
     model.model.push_to_hub(f"{username}/{model_name}", token=token, private=False)
     tokenizer.push_to_hub(f"{username}/{model_name}", token=token, private=False)
     
